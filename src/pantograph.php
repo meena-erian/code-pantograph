ccaa493a4e4032662663f54ecb5597af4ab516ef
Meena Erian <meena5.erian@gmail.com>
2020-02-24 20:38:47 +0200

    Update pantograph.php

--- src/pantograph.php
+++ src/pantograph.php
@@ -1,11 +1,35 @@
 <?php
 
 
-$SECRET = "****************************************";
+/*
+    Configurations
+        $SECRET : This has to be the secret set for the Github WebHook and
+            it's used by Github's Hookshot to sign the payload and then it's
+            uesd here to verify that it's sent by the configured hook.
+        
+        $LOCAL_PATH : The local path to the directory on your http server to 
+            which the code-pantograph will write. it can be an absolute realpath
+            or a path relative to the location of the php script.
+            
+        $REPO_PATH : The path to the directory from which the code-pantograph
+            will read, relative to the repository's main directory. For example,
+            this will be src to synchronize all files in the src directory 
+            
+        $REPO_BRANCH : The branch from which code-pantograph will read. 
+            Default: master.
+            
+        $USER_ACCESS_TOKEN : Github user access token. Used to let the script
+            access the repositoryon behalf of a user. Use this only if the 
+            repository from which code-pantograph will read is a private repository.
+            See https://github.com/settings/tokens for more information
+            Use the repo scope to allow the script to read private repositoies
+*/
+
+$SECRET = "********Github WebHook Signature's Secret*******";
 $LOCAL_PATH = "..";
 $REPO_PATH = "src";
 $REPO_BRANCH = "master";
-$USER_ACCESS_TOKEN = "########################################";
+//$USER_ACCESS_TOKEN = "########################################";
 
 
 define("DEBUG");
